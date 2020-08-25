1864a94465c8041a52f3803fe9bb5606eb77b5bb
Nahuel Hourcade <nahuel.h.l@gmail.com>
2020-08-25 16:28:41 -0300

    Agrega botón "arriba"

---
+++ script.js
@@ -0,0 +1,22 @@
+// Get the button
+var btnTop = document.querySelector(".btn-top");
+if (btnTop) {
+	btnTop.style.display = "none";
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			btnTop.style.display = "block";
+		} else {
+			btnTop.style.display = "none";
+		}
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+}
